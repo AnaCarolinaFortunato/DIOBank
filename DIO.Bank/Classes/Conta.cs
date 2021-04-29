46e6eb371d103e29a9c120c0d2001d37498207d1
AnaCarolinaFortunato <anacarolinafortunatosantos@gmail.com>
2021-04-29 12:53:58 -0300

    Adicionar arquivos de projeto.

---
+++ DIO.Bank/Classes/Conta.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DIO.Bank
+{
+    public class Conta
+    {
+        public Conta(TipoConta tipoConta, string nome, double saldo, double credito)
+        {
+            TipoConta = tipoConta;
+            Nome = nome;
+            Saldo = saldo;
+            Credito = credito;
+        }
+
+        private TipoConta TipoConta { get;  set; }
+        private string Nome { get;  set; }
+        private double Saldo { get;  set; }
+        private double Credito { get;  set; }
+        
+        //validacao de saldo
+        public bool Sacar(double valorSaque)
+        {
+            if (Saldo - valorSaque < (Credito * -1))
+            {
+                Console.WriteLine("Saldo insuficiente!");
+                return false;
+            }
+            Saldo = Saldo - valorSaque;
+
+            Console.WriteLine("Saldo atual da conta de {0} é {1}", Nome, Saldo);
+            return true;
+        }
+
+        public void Depositar (double valorDeposito)
+        {
+            Saldo = Saldo + valorDeposito;
+
+            Console.WriteLine("Saldo atual da conta de {0} é {1}", Nome, Saldo);
+
+        }
+
+        public void Transferir(double valorTranferencia, Conta contaDestino)
+        {
+            if (Sacar(valorTranferencia))
+            {
+                contaDestino.Depositar(valorTranferencia);
+            }
+            Saldo = Saldo - valorTranferencia;
+
+            Console.WriteLine("Saldo atual da conta de {0} é {1}", Nome, Saldo);
+           
+        }
+
+        public override string ToString()
+        {
+            string retorno = "";
+            retorno += " TipoConta " + TipoConta + " | ";
+            retorno += " Nome " + Nome + " | ";
+            retorno += " Saldo " + Saldo + " | ";
+            retorno += " Crédito " + Credito + " | ";
+            return retorno;
+        }
+    }
+}
